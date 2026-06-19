c3370f624a9c8d3c49ab0a6cbb5ea3ac113f16f7
Heitor110 <heitor14.romagnoli@gmail.com>
2026-06-19 08:20:27 -0300

    Iniciando o sistema...

---
+++ SysEtec/SysEtec/Form1.cs
@@ -0,0 +1,25 @@
+namespace SysEtec
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saídaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
